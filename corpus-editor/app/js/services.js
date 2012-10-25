0e455382bb209a7e4072079e41d7ccb1462ba311
yingrui <twer@yingrui-laptop-2.local>
2012-10-25 23:13:18 +0800

    init corpus editor.

---
+++ corpus-editor/app/js/services.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/* Services */
+
+angular.module('phonecatServices', ['ngResource']).
+    factory('Phone', function($resource){
+  return $resource('phones/:phoneId.json', {}, {
+    query: {method:'GET', params:{phoneId:'phones'}, isArray:true}
+  });
+});
