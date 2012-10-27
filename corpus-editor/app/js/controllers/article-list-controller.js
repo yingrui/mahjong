0d134d1a23793f2dee10e772ff32b8ca01bc3696
Yingrui <yingrui.f@gmail.com>
2012-10-27 16:01:57 +0800

    delete old css layout.

--- corpus-editor/app/js/controllers/article-list-controller.js
+++ corpus-editor/app/js/controllers/article-list-controller.js
@@ -3,8 +3,14 @@
 /* Controllers */
 
 function ArticleListCtrl($scope, ArticleRepository) {
-  $scope.articles = ArticleRepository.query();
-  $scope.orderProp = 'date';
+    $scope.articles = ArticleRepository.query();
+    $scope.sortReverse = function() {
+        var sortBy = $scope.orderProp;
+        if("date" == sortBy) {
+            return true;
+        }
+        return false;
+    };
 }
 
 ArticleListCtrl.$inject = ['$scope', 'ArticleRepository'];
