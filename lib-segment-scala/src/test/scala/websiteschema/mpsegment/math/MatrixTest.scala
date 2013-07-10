10c903e621951504c40850740bb57fc0da903af0
yingrui <yingrui.f@gmail.com>
2013-07-11 00:01:44 +0800

    [FYR] matrix arithmetric: add, multiply

--- lib-segment-scala/src/test/scala/websiteschema/mpsegment/math/MatrixTest.scala
+++ lib-segment-scala/src/test/scala/websiteschema/mpsegment/math/MatrixTest.scala
@@ -23,15 +23,71 @@ class MatrixTest {
     Assert.assertTrue(m.flatten.forall(doubleEqual(_, 3D)))
   }
 
+  @Test
+  def should_throw_exception_when_matrix_form_is_not_match() {
+    try {
+      val m = Matrix(2, 2, List(1D, 2D, 1D, 2D)) + Matrix(3, 3, List(2D, 1D, 2D, 1D))
+      Assert.fail()
+    }
+    catch {
+      case _: Throwable =>
+    }
+  }
+
+  @Test
+  def should_return_all_elements_in_specified_row(){
+    val m = Matrix(2, 3, List(1D, 1D, 1D, 2D, 2D, 2D))
+    Assert.assertEquals(3, m.row(0).length)
+    Assert.assertTrue(m.row(0).forall(doubleEqual(_, 1D)))
+  }
+
+  @Test
+  def should_return_all_elements_in_specified_col(){
+    val m = Matrix(2, 3, List(1D, 2D, 3D, 1D, 2D, 3D))
+    val col: Array[Double] = m.col(0)
+    Assert.assertEquals(2, col.length)
+    Assert.assertTrue(col.forall(doubleEqual(_, 1D)))
+  }
+
+  @Test
+  def should_return_right_value_when_multiply_number() {
+    val m = Matrix(2, 2, List(1D, 1D, 1D, 1D)) x 4D
+    Assert.assertTrue(m.flatten.forall(doubleEqual(_, 4D)))
+  }
+
+  @Test
+  def should_return_right_value_when_multiply_matrices() {
+    val m = Matrix(2, 2, List(1D, 1D, 2D, 2D))
+    val n = Matrix(2, 2, List(1D, 1D, 2D, 2D))
+    val matrix = m x n
+    Assert.assertTrue(doubleEqual(3D, matrix(0,0)))
+    Assert.assertTrue(doubleEqual(3D, matrix(0,1)))
+    Assert.assertTrue(doubleEqual(6D, matrix(1,0)))
+    Assert.assertTrue(doubleEqual(6D, matrix(1,1)))
+  }
+
   def doubleEqual(d: Double, other: Double) = (d - other) < 0.00000001D && (d - other) > -0.00000001D
 }
 
 trait Matrix {
-  def +(n: Int): Matrix
+  def +(n: Double): Matrix
 
   def +(m: Matrix): Matrix
 
+  def x(n: Double): Matrix
+
+  def x(m: Matrix): Matrix
+
   def flatten: Array[Double]
+
+  def row(i: Int): Array[Double]
+
+  def col(i: Int): Array[Double]
+
+  val row: Int
+  val col: Int
+
+  def apply(i: Int, j:Int): Double
 }
 
 object Matrix {
@@ -42,12 +98,37 @@ object Matrix {
     (for (i <- 0 until data.length) yield op(data(i), other(i))).toArray
 }
 
-class DenseMatrix(row: Int, col: Int, data: Array[Double]) extends Matrix {
+class DenseMatrix(val row: Int, val col: Int, data: Array[Double]) extends Matrix {
 
-  def +(n: Int) = new DenseMatrix(row, col, data.map(_ + n))
+  def +(n: Double) = new DenseMatrix(row, col, data.map(_ + n))
 
-  def +(m: Matrix) = new DenseMatrix(row, col, Matrix.arithmetic(flatten, m.flatten, (a,b) => a + b))
+  def +(m: Matrix) = {
+    assert(row == m.row && col == m.col)
+    new DenseMatrix(row, col, Matrix.arithmetic(flatten, m.flatten, (a,b) => a + b))
+  }
+
+  def x(n: Double) = new DenseMatrix(row, col, data.map(_ * n))
+
+  def x(m: Matrix) = {
+    assert(col == m.row)
+    val result = Matrix(row, m.col)
+    var index = 0;
+    for (i <- 0 until row) {
+      for (j <- 0 until m.col) {
+        result.flatten(index) = Matrix.arithmetic(row(i), col(j), (a,b) => a*b).sum
+        index += 1
+      }
+    }
+    result
+  }
+
+  implicit def seqToArray(seq: Seq[Double]) = seq.toArray
+
+  def row(i: Int): Array[Double] = for (index <- 0 until data.length; if (index < (i+1)*col) && index >= i * col) yield data(index)
+
+  def col(i: Int): Array[Double] = for (index <- 0 until data.length; if (index % col == i)) yield data(index)
+
+  def apply(i: Int, j:Int): Double = data(row * i + j)
 
   def flatten = data
-
 }
