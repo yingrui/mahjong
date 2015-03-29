ea1eadf43977939b32507b142f3b841252f0e755
yingrui <yingrui.f@gmail.com>
2015-03-28 20:29:32 -0400

    refactor softmax regression classifier test; rename dataset;

--- lib-segment/src/test/scala/websiteschema/mpsegment/neural/regression/SoftmaxRegressionTest.scala
+++ lib-segment/src/test/scala/websiteschema/mpsegment/neural/regression/SoftmaxRegressionTest.scala
@@ -1,10 +1,11 @@
-package websiteschema.mpsegment.neural
+package websiteschema.mpsegment.neural.regression
 
 import java.lang.Math.{abs, log}
 import java.util.Date
 
 import org.scalatest.{FunSuite, Matchers}
 import websiteschema.mpsegment.math.Matrix
+import websiteschema.mpsegment.neural.{Layer, BPLayer, SoftmaxLayer}
 import websiteschema.mpsegment.util.Logger._
 
 import scala.io.Source
@@ -16,8 +17,8 @@ class SoftmaxRegressionTest extends FunSuite with Matchers {
 
   private def loadData(dataSet: String) = {
     val classLoader = getClass.getClassLoader()
-    val inputSource = Source.fromURL(classLoader.getResource(s"softmax/${dataSet}X.txt"))
-    val outputSource = Source.fromURL(classLoader.getResource(s"softmax/${dataSet}Y.txt"))
+    val inputSource = Source.fromURL(classLoader.getResource(s"wisconsin-breast-cancer/${dataSet}X.txt"))
+    val outputSource = Source.fromURL(classLoader.getResource(s"wisconsin-breast-cancer/${dataSet}Y.txt"))
 
     val inputs = inputSource.getLines().map(line => Matrix(line.split("\\s+").map(_.toDouble))).toArray
 
@@ -72,34 +73,47 @@ class SoftmaxRegressionTest extends FunSuite with Matchers {
     weight
   }
 
+  private def classify(classifier: Layer, testInput: Matrix): Matrix = {
+    val actualOutput = classifier computeOutput testInput
+    if (actualOutput(0, 0) > actualOutput(0, 1)) {
+      actualOutput(0, 0) = 1.0D
+      actualOutput(0, 1) = 0.0D
+    } else {
+      actualOutput(0, 0) = 0.0D
+      actualOutput(0, 1) = 1.0D
+    }
+    actualOutput
+  }
+
   test("train softmax classifier") {
-    println("train set size is " + trainDataSet.size + " test set size is " + testDataSet.size + " " + new Date())
+    val tic = System.currentTimeMillis()
+    println("train set contains " + trainDataSet.size + " samples, test set contains " + testDataSet.size + " samples.")
 
     val weight = train
 
     val classifier = SoftmaxLayer(weight)
 
-    var error = 0D
-    testDataSet.foreach(data => {
+    val error = testDataSet.map(data => {
       val testInput = data._1
       val expectedOutput = data._2
 
-      val actualOutput = classifier computeOutput testInput
-      if(actualOutput(0, 0) > actualOutput(0, 1)) {
-        actualOutput(0, 0) = 1.0D
-        actualOutput(0, 1) = 0.0D
-      } else {
-        actualOutput(0, 0) = 0.0D
-        actualOutput(0, 1) = 1.0D
-      }
+      val actualOutput: Matrix = classify(classifier, testInput)
 
-      if((expectedOutput - actualOutput).map(abs(_)).flatten.sum > 0) error += 1.0D
-    })
+      if((expectedOutput - actualOutput).map(abs(_)).flatten.sum > 0)
+        1.0D
+      else
+        0.0D
+    }).sum
 
     val numberOfSamples = testDataSet.size.toDouble
+    val accuracy = 1.0 - error / numberOfSamples
+    accuracy should be > 0.92
     println("error = " + error + " total = " + numberOfSamples)
-    println("accuracy = " + (1.0 - error / numberOfSamples))
-    println(new Date())
+    println("accuracy = " + accuracy)
+    val toc = System.currentTimeMillis()
+    val second = (toc - tic) / 1000
+    println(s"elapsed time: ${second}s")
   }
+
 
 }
