3f9628c57d19a8bb230a99040f979dff849a0df3
yingrui <yingrui.f@gmail.com>
2017-02-06 18:35:50 +0800

    @yingrui: add more label for disambugation

--- lib-segment/src/main/scala/me/yingrui/segment/core/disambiguation/DisambiguationToSerialLabels.scala
+++ lib-segment/src/main/scala/me/yingrui/segment/core/disambiguation/DisambiguationToSerialLabels.scala
@@ -4,9 +4,13 @@ import me.yingrui.segment.core.{SegmentResult, Word}
 import me.yingrui.segment.tools.accurary.SegmentResultCompareHook
 
 /**
-  * S 一个词属于正确的词的一部分 (Separated)
+  * SB 一个词属于正确的词的开始 (Separated word Beginning part)
+  * SE 一个词属于正确的词的开始 (Separated word Ending part)
+  * SH 一个双字词，应该分为两部分，并且将两个字分别加入前后两个词 (Split Half)
   * LC 一个词的最后一个字是下一个词的首字 (Last Character)
   * LL 上一个词的最后一个字，应当属于当前词 (Last word's Last character)
+  * U  当前词由两个单字词组成 (Union Words)
+  * A  默认标记
   * @param expect
   * @param actual
   */
@@ -23,37 +27,75 @@ class DisambiguationToSerialLabels(expect: SegmentResult, actual: SegmentResult)
   }
 
   override def foundError(expectWord: Word, actualWord: Word, expectWordIndex: Int, actualWordIndex: Int): Unit = {
+    addLabel(expectWord, actualWord, expectWordIndex, actualWordIndex)
+  }
+
+  def addLabel(expectWord: Word, actualWord: Word, expectWordIndex: Int, actualWordIndex: Int): String = {
     if (isExpectWordContainsActualWord(expectWord, actualWord)) {
       if (isLastWordLastCharacterBelongToThisWord(expectWord, actualWord)) {
         add(actualWord.name, "LL")
+      } else if (expectWord.name.startsWith(actualWord.name)) {
+        add(actualWord.name, "SB")
+      } else if (expectWord.name.endsWith(actualWord.name)) {
+        add(actualWord.name, "SE")
       } else {
-        add(actualWord.name, "S")
+        add(actualWord.name, "A")
       }
     } else {
-      if (isActualWordContainsExpectedWord(expectWord, actualWord) && isLastCharacterBelongToNextWord(expectWord, actualWord)) {
+      if (isActualWordStartsWithExpectedWordAndLastCharacterBelongsToNextWord(expectWord, actualWord, expectWordIndex)) {
         add(actualWord.name, "LC")
+      } else if (isTwoCharactersWord(actualWord) && isActualWordComposedOfTwoExpectedWords(expectWord, actualWord, expectWordIndex)) {
+        add(actualWord.name, "U")
+      } else if (isTwoCharactersWord(actualWord) && shouldActualWordSeparateToJoinPreviousAndNextWords(expectWord, actualWord, expectWordIndex)) {
+        add(actualWord.name, "SH")
+      } else {
+        add(actualWord.name, "A")
       }
     }
-
   }
 
-  def isLastWordLastCharacterBelongToThisWord(expectWord: Word, actualWord: Word): Boolean = {
-    serialLabels.last._2 == "LC" && expectWord.name.endsWith(actualWord.name)
+  def shouldActualWordSeparateToJoinPreviousAndNextWords(expectWord: Word, actualWord: Word, expectWordIndex: Int): Boolean = {
+    isNotLastWord(expectWordIndex) && expectWord.name.endsWith(actualWord.name.substring(0, 1)) && getNextExpectWord(expectWordIndex).startsWith(actualWord.name.substring(1))
   }
 
-  def isLastCharacterBelongToNextWord(expectWord: Word, actualWord: Word): Boolean = {
+  private def isTwoCharactersWord(actualWord: Word): Boolean = {
+    actualWord.length == 2
+  }
+
+  private def isActualWordComposedOfTwoExpectedWords(expectWord: Word, actualWord: Word, expectWordIndex: Int): Boolean = {
+    isNotLastWord(expectWordIndex) && actualWord.name == (expectWord.name + getNextExpectWord(expectWordIndex))
+  }
+
+  private def isNotLastWord(expectWordIndex: Int): Boolean = expectWordIndex + 1 < expect.length()
+
+  private def getNextExpectWord(index: Int): String = expect.getWord(index + 1)
+
+  private def isActualWordStartsWithExpectedWordAndLastCharacterBelongsToNextWord(expectWord: Word, actualWord: Word, expectWordIndex: Int): Boolean = {
+    isNotLastWord(expectWordIndex) && isActualWordContainsExpectedWord(expectWord, actualWord) && isLastCharacterBelongToNextWord(expectWord, actualWord) && getNextExpectWord(expectWordIndex).length > 1
+  }
+
+  private def isLastWordLastCharacterBelongToThisWord(expectWord: Word, actualWord: Word): Boolean = {
+    isLastLabel("LC") && expectWord.name.endsWith(actualWord.name)
+  }
+
+  def isLastLabel(label: String): Boolean = {
+    serialLabels.length > 0 && serialLabels.last._2 == label
+  }
+
+  private def isLastCharacterBelongToNextWord(expectWord: Word, actualWord: Word): Boolean = {
     actualWord.name.startsWith(expectWord.name) && actualWord.length == expectWord.length + 1
   }
 
-  def isActualWordContainsExpectedWord(expectWord: Word, actualWord: Word): Boolean = {
+  private def isActualWordContainsExpectedWord(expectWord: Word, actualWord: Word): Boolean = {
     actualWord.name.contains(expectWord.name)
   }
 
-  def isExpectWordContainsActualWord(expectWord: Word, actualWord: Word): Boolean = {
+  private def isExpectWordContainsActualWord(expectWord: Word, actualWord: Word): Boolean = {
     expectWord.name.contains(actualWord.name)
   }
 
-  private def add(word: String, label: String): Unit = {
+  private def add(word: String, label: String): String = {
     serialLabels = serialLabels :+ (word, label)
+    label
   }
 }
--- lib-segment/src/test/scala/me/yingrui/segment/core/disambiguation/DisambiguationToSerialLabelsTest.scala
+++ lib-segment/src/test/scala/me/yingrui/segment/core/disambiguation/DisambiguationToSerialLabelsTest.scala
@@ -6,29 +6,52 @@ import org.scalatest.{FunSuite, Matchers}
 
 class DisambiguationToSerialLabelsTest extends FunSuite with Matchers {
 
-  test("should label separated words as S") {
-    val expect = convertToSegmentResult("19980101-01-003-002/m  保护/v 人体/n 健康/n")
-    val actual = convertToSegmentResult("19980101-01-003-002/m  保护/v 人/n 体/j 健康/n")
+  def compareSegmentResult(expectedSegmentResult: String, actualSegmentResult: String): DisambiguationToSerialLabels = {
+    val expect = convertToSegmentResult(expectedSegmentResult)
+    val actual = convertToSegmentResult(actualSegmentResult)
 
     val hooker = new DisambiguationToSerialLabels(expect, actual)
-    val comparator = new SegmentResultComparator(hooker)
-    comparator.compare(expect, actual)
+    new SegmentResultComparator(hooker).compare(expect, actual)
+    hooker
+  }
 
-    hooker.serialLabels.size should be (4)
-    hooker.serialLabels(1)._2 should be ("S")
-    hooker.serialLabels(2)._2 should be ("S")
+  test("should label separated words as SB and SE") {
+    val hooker = compareSegmentResult(
+      "19980101-01-003-002/m  保护/v 人体/n 健康/n",
+      "19980101-01-003-002/m  保护/v 人/n 体/j 健康/n"
+    )
+
+    hooker.serialLabels.size should be(4)
+    hooker.serialLabels.map(_._2) should be(List("A", "SB", "SE", "A"))
   }
 
   test("when the word's last character belongs to next word then label it as LC") {
-    val expect = convertToSegmentResult("19980101-01-003-002/m  精神/n 病人/n")
-    val actual = convertToSegmentResult("19980101-01-003-002/m  精神病/n 人/n")
+    val hooker = compareSegmentResult(
+      "19980101-01-003-002/m  精神/n 病人/n",
+      "19980101-01-003-002/m  精神病/n 人/n"
+    )
 
-    val hooker = new DisambiguationToSerialLabels(expect, actual)
-    val comparator = new SegmentResultComparator(hooker)
-    comparator.compare(expect, actual)
+    hooker.serialLabels.size should be(2)
+    hooker.serialLabels.map(_._2) should be(List("LC", "LL"))
+  }
 
-    hooker.serialLabels.size should be (2)
-    hooker.serialLabels(0)._2 should be ("LC")
-    hooker.serialLabels(1)._2 should be ("LL")
+  test("when two single character words be put into one word then label it as U") {
+    val hooker = compareSegmentResult(
+      "19980101-01-003-002/m  很/d 美/a 的/u",
+      "19980101-01-003-002/m  很/d 美的/nt"
+    )
+
+    hooker.serialLabels.size should be(2)
+    hooker.serialLabels.map(_._2) should be(List("A", "U"))
+  }
+
+  test("when two single character words should be separate and join the previous and next word then label it as SH") {
+    val hooker = compareSegmentResult(
+      "19980101-01-003-002/m  神经病变/n 成为/v",
+      "19980101-01-003-002/m  神经病/d 变成/v 为/d"
+    )
+
+    hooker.serialLabels.size should be(3)
+    hooker.serialLabels.map(_._2) should be(List("SB", "SH", "SE"))
   }
 }
