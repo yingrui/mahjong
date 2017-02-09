f5a6253ef718a47d350970c1e51102f168af1974
yingrui <yingrui.f@gmail.com>
2017-02-09 19:13:20 +0800

    @yingrui: implement crf disambiguation filter, separate U to two words.

--- lib-segment/src/main/scala/me/yingrui/segment/filter/disambiguation/CRFDisambiguationFilter.scala
+++ lib-segment/src/main/scala/me/yingrui/segment/filter/disambiguation/CRFDisambiguationFilter.scala
@@ -6,9 +6,24 @@ import me.yingrui.segment.filter.AbstractSegmentFilter
 class CRFDisambiguationFilter(classfier: CRFClassifier) extends AbstractSegmentFilter {
 
   override def doFilter(): Unit = {
-    val labels = classfier.findBestLabels(currentSegmentResult)
-    labels
+    val words = currentSegmentResult()
+    val labels = classfier.findBestLabels(words)
+    labels.zipWithIndex.foreach(_ match {
+      case (label, index) => process(label, index, labels, words)
+      case _ =>
+    })
   }
 
-  private def currentSegmentResult = this.segmentResult.map(_.name)
+  private def process(label: String, index: Int, labels: Seq[String], words: Seq[String]): Unit = {
+    label match {
+      case "U" => {
+        val pos = segmentResult.getPOS(index)
+        separateWordAt(index, pos, pos)
+      }
+      case "A" =>
+      case _ =>
+    }
+  }
+
+  private def currentSegmentResult() = this.segmentResult.map(_.name)
 }
--- lib-segment/src/test/scala/me/yingrui/segment/filter/disambiguation/CRFDisambiguationFilterTest.scala
+++ lib-segment/src/test/scala/me/yingrui/segment/filter/disambiguation/CRFDisambiguationFilterTest.scala
@@ -1,13 +1,32 @@
 package me.yingrui.segment.filter.disambiguation
 
+import me.yingrui.segment.core.SegmentResult
+import me.yingrui.segment.core.disambiguation.DisambiguationToSerialLabels._
+import me.yingrui.segment.crf.CRFClassifier
+import me.yingrui.segment.dict.POSUtil._
 import me.yingrui.segment.tools.PFRCorpusLoader._
-import org.scalatest.{FunSuite, Ignore}
+import org.mockito.Matchers._
+import org.mockito.Mockito._
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{FunSuite, Matchers}
 
-@Ignore
-class CRFDisambiguationFilterTest extends FunSuite {
+class CRFDisambiguationFilterTest extends FunSuite with Matchers with MockitoSugar {
 
   test("should separate union words") {
     val segmentResult = convertToSegmentResult("19980101-01-003-002/m  很/d 美的/nt")
+    val labels = Array(LABEL_A, LABEL_U)
+
+    createFilter(segmentResult, labels).filtering()
+
+    segmentResult.map(_.name) should be(Array("很", "美", "的"))
+    segmentResult.map(_.pos) should be(Array(POS_D, POS_NT, POS_NT))
   }
 
+  private def createFilter(segmentResult: SegmentResult, labels: Array[String]): CRFDisambiguationFilter = {
+    val classifier = mock[CRFClassifier]
+    when(classifier.findBestLabels(any())).thenReturn(labels)
+    val filter = new CRFDisambiguationFilter(classifier)
+    filter.setSegmentResult(segmentResult)
+    filter
+  }
 }
